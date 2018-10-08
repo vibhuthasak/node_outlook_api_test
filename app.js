99064b2b2f6935522c088a321c506da65c2518d6
Vibhutha Kumarage <vkartz@gmail.com>
2018-10-08 12:06:59 +0530

    Mail API added

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ require('dotenv').config();
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var authorize = require('./routes/authorize');
+var mail = require('./routes/mail');
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/authorize', authorize);
+app.use('/mail', mail);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/mail.js
@@ -0,0 +1,39 @@
+router.get('/', async function(req, res, next) {
+    let parms = { title: 'Inbox', active: { inbox: true } };
+
+    const accessToken = await authHelper.getAccessToken(req.cookies, res);
+    const userName = req.cookies.graph_user_name;
+
+    if (accessToken && userName) {
+        parms.user = userName;
+
+        // Initialize Graph client
+        const client = graph.Client.init({
+            authProvider: (done) => {
+                done(null, accessToken);
+            }
+        });
+
+        try {
+            // Get the 10 newest messages from inbox
+            const result = await client
+                .api('/me/mailfolders/inbox/messages')
+                .top(10)
+                .select('subject,from,receivedDateTime,isRead')
+                .orderby('receivedDateTime DESC')
+                .get();
+
+            parms.messages = result.value;
+            res.render('mail', parms);
+        } catch (err) {
+            parms.message = 'Error retrieving messages';
+            parms.error = { status: `${err.code}: ${err.message}` };
+            parms.debug = JSON.stringify(err.body, null, 2);
+            res.render('error', parms);
+        }
+
+    } else {
+        // Redirect to home
+        res.redirect('/');
+    }
+});
